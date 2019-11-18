7673a6e7951d5ac5c57e46d488de99fb8b5e3c43
SteelBoshido99 <Dev+SteelBoshido99@users.noreply.github.com>
2019-11-18 15:52:42 +0000

    Added some Health and UI

---
+++ Assets/Scripts/GameUI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameUI : MonoBehaviour
+{
+    public Slider healthBar;
+    public Text scoreText;
+
+    public int playerScore = 0;
+
+
+    private void OnEnable()
+    {
+        Player.OnUpdateHealth += UpdateHealthBar;
+        AddScore.OnSendScore -= UpdateScore;
+    }
+
+
+    private void OnDisable()
+    {
+        Player.OnUpdateHealth -= UpdateHealthBar;
+        AddScore.OnSendScore -= UpdateScore;
+    }
+
+    private void UpdateHealthBar(int health)
+    {
+        healthBar.value = health;
+    }
+
+    private void UpdateScore(int theScore)
+    {
+        playerScore += theScore;
+        scoreText.text = "SCORE:" + playerScore.ToString();
+    }
+
+}
