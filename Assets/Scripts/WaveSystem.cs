51efec26c58353cafaa668aa56c98e062214e020
SteelBoshido99 <Dev+SteelBoshido99@users.noreply.github.com>
2019-12-09 17:19:31 +0000

    Update 9
    Wavesystem made

--- Assets/Scripts/WaveSystem.cs
+++ Assets/Scripts/WaveSystem.cs
@@ -4,32 +4,76 @@ using UnityEngine;
 
 public class WaveSystem : MonoBehaviour
 {
-       
-    public int ZomNumber;
-    public int ZomHealth;
-    public int ZomDamage;
-    public int WaveNum;
-    public int SpawnerNum;
-    int NowWave = 10;
+
+    public Wave[] waves;
+
+    public float randomPos = 2; // This states the position in which the Zombies spawn between
+
+    public int currentWaveIndex = 0; // This states the number of waves 
+    public int remaining = 0; // This monitors the number of zombies remaining
 
 
-    public WaveSystem(int TheZomNum, int theZomHealth, int theWaveNum)
+    //public int ZomNumber;
+    //public int ZomHealth;
+    //public int ZomDamage;
+    //public int WaveNum;
+    //public int SpawnerNum;
+    //int NowWave = 10;
+
+
+    //public WaveSystem(int TheZomNum, int theZomHealth, int theWaveNum)
+    //{
+    //    ZomNumber = TheZomNum;
+    //    ZomHealth = theZomHealth;
+    //    WaveNum = theWaveNum;
+    //}
+
+
+    //Start is called before the first frame update
+    void Start()
     {
-        ZomNumber = TheZomNum;
-        ZomHealth = theZomHealth;
-        WaveNum = theWaveNum;
+        //WaveSystem[] NewWave = new WaveSystem[NowWave];
+        Spawn();
+     }
+
+
+    public void OnZombieKilled()
+    {
+        remaining--;
+
+        if (remaining <= 0)
+        {
+            currentWaveIndex++;
+            Spawn();
+        }
     }
 
-    // Start is called before the first frame update
-    //void Start()
-    //{
+    void Spawn() //A method that contains the spawning mechanism
+    {
+        remaining = waves[currentWaveIndex].amount;
 
-    ////    WaveSystem[] NewWave = new WaveSystem[NowWave];
-    ////    NewWave[0] = 
+        for (int i = 0; i < waves[currentWaveIndex].amount; i++)
+        {
+            float x = Random.Range(-randomPos, randomPos);
+            float y = Random.Range(-randomPos, randomPos);
 
-       
-        
-    ////}
+            Vector3 point = new Vector3(x, y, 0);
 
-    
+
+            HealthSystem health =  Instantiate(waves[0].prefab, transform.position + point, transform.rotation, transform).GetComponent<HealthSystem>();
+            //---The syntax above spawns in the Zombie once it gets a message from the dead zombie that it's dead---//
+            health.onDie.AddListener(OnZombieKilled);
+
+        }
+    }
 }
+
+[System.Serializable]
+public struct Wave
+{
+    public int amount;
+    public GameObject prefab;
+}
+
+
+
