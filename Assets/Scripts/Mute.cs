031de5d34170f8d0897c66e29f9a6388f7615907
SteelBoshido99 <Owner+SteelBoshido99@users.noreply.github.com>
2019-12-30 17:35:47 +0000

    Update 14
    Possible mute button as been added

---
+++ Assets/Scripts/Mute.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Mute : MonoBehaviour
+{
+    public static bool AudioMute = false;
+
+   
+
+
+    public void Muter()
+    {
+        if (AudioMute)
+        {
+            Noise();
+        }
+        else
+        {
+            Silence();
+        }
+    }
+
+    private void Silence()
+    {
+        AudioListener.pause = true;
+    }
+
+    private void Noise()
+    {
+        AudioListener.pause = false;
+    }
+
+
+}
